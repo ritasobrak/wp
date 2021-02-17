51440bf4eeffe7beceec9d59676703d95eaa4b3d
sobrakmargarita1 <margarita.sobrak@khk.ee>
2021-02-17 15:58:59 +0200

    modified file `wordfence-waf.php`

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/home/sobrakmargarita1/public_html/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/home/sobrakmargarita1/public_html/wordpress/wp-content/wflogs/');
+	include_once '/home/sobrakmargarita1/public_html/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
